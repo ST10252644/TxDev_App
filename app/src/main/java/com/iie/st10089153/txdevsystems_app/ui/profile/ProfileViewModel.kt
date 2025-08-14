444567181c187274f4f6417615565835a99a75f5
ST10320489 <st10320489@vcconnect.edu.za>
2025-08-14 15:09:47 +0200

    Navbar, Nav header and errors fixed
    we fixed it

--- app/src/main/java/com/iie/st10089153/txdevsystems_app/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/iie/st10089153/txdevsystems_app/ui/profile/ProfileViewModel.kt
@@ -1,4 +1,13 @@
 package com.iie.st10089153.txdevsystems_app.ui.profile
 
-class ProfileViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Profile Fragment"
+    }
+    val text: LiveData<String> = _text
 }
