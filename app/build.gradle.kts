c26d54be17bb5b7e5932d61c72b4edae88ad3450
ST10322054 <126609744+ST10322054@users.noreply.github.com>
2025-08-14 13:52:52 +0200

    fonts
    man did a thing to a man with a bing account

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,12 +51,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-
-    //Login API Authentication & access token
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
-
-
 }
--- app/src/main/java/com/iie/st10089153/txdevsystems_app/ui/login/LoginResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.iie.st10089153.txdevsystems_app.ui.login
-
-data class LoginResponse(
-    val access_token: String,
-    val token_type: String,
-    val expires_in: Int,
-    val refresh_token: String? = null
-)
