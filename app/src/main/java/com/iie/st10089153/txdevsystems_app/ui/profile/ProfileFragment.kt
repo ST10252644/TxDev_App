f96403b0cb229fcc77d6ecc1059005c4da509659
Inge <dafelinge@gmail.com>
2025-08-16 10:32:45 +0200

    Profile View and Edit
    With new drawables and basic UI components

--- app/src/main/java/com/iie/st10089153/txdevsystems_app/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/iie/st10089153/txdevsystems_app/ui/profile/ProfileFragment.kt
@@ -1,4 +1,47 @@
-package com.iie.st10089153.txdevsystems_app.ui.profile
+package com.iie.st10089153.fragments
 
-class ProfileFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.iie.st10089153.txdevsystems_app.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Create a simple container view for hosting profile fragments
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Automatically show the ViewProfileFragment when ProfileFragment loads
+        if (savedInstanceState == null) {
+            showViewProfileFragment()
+        }
+    }
+
+    private fun showViewProfileFragment() {
+        val viewProfileFragment = ViewProfileFragment()
+
+        childFragmentManager.beginTransaction()
+            .replace(R.id.profile_fragment_container, viewProfileFragment)
+            .commit()
+    }
+
+    // Method to programmatically switch to edit profile
+    fun showEditProfileFragment() {
+        val editProfileFragment = UpdateProfileFragment()
+
+        childFragmentManager.beginTransaction()
+            .replace(R.id.profile_fragment_container, editProfileFragment)
+            .addToBackStack("ViewProfile")
+            .commit()
+    }
 }
